ecf9b62d745782b8594cbeac8e9c6ce1c30c7a76
netonblpb <skullbog@gmail.com>
2022-02-03 14:11:40 +0300

    Task4 Complete

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Task 4: Найти максимальное из трех чисел
+
+int[] numbers = new int[] {1, 6, 3};
+int max = 0;
+
+foreach (var x in numbers)
+{
+    if (x > max)
+    max = x;    
+}
+
+Console.WriteLine(max);
